b2d78548bf794f60e4709694fcb80994c638f790
komahanb <komahan.cool@gmail.com>
2015-08-28 03:12:38 -0400

    Added updates for q,qdot,ddotdot

--- src/aaa_solver_utils.f90
+++ src/aaa_solver_utils.f90
@@ -3,23 +3,65 @@ use settings
 implicit none
 
 private
-public get_extrapolated_q
+public get_updated_q, get_updated_q_dot, get_updated_q_double_dot,&
+     & get_extrapolated_q
 
 contains
+
+!----------------------------------------------------
+! updates the q vector value with the computed update
+!----------------------------------------------------
+function get_updated_q(old_q, del_q) result(new_q)
+real(dp), intent(in)           :: del_q(:)
+real(dp), intent(in)           :: old_q(:)
+real(dp)                       :: new_q(size(old_q))
+
+new_q(:) = old_q(:) + del_q(:)
+
+end function
+
+!--------------------------------------------------------
+! updates the q_dot vector value with the computed update
+!--------------------------------------------------------
+function get_updated_q_dot(del_T, old_q_dot, del_q_dot) result(new_q_dot)
+real(dp), intent(in)           :: del_q_dot(:)
+real(dp), intent(in)           :: old_q_dot(:)
+real(dp), intent(in)           :: del_T
+
+real(dp)                       :: alpha0
+real(dp)                       :: new_q_dot(size(old_q_dot))
+
+new_q_dot(:) = old_q_dot(:) + alpha0*del_q_dot(:)/del_t
+
+end function
+!---------------------------------------------------------------
+! updates the q_double_dot vector value with the computed update
+!---------------------------------------------------------------
+function get_updated_q_double_dot(del_T, old_q_double_dot, del_q_double_dot) result(new_q_double_dot)
+real(dp), intent(in)           :: del_q_double_dot(:)
+real(dp), intent(in)           :: old_q_double_dot(:)
+real(dp), intent(in)           :: del_T
+
+real(dp)                       :: beta0
+real(dp)                       :: new_q_double_dot(size(old_q_double_dot))
+
+new_q_double_dot(:) = old_q_double_dot(:) + beta0*del_q_double_dot(:)/del_t**2
+
+end function
 
 !--------------------------------------------------------------------------
 ! returns the extrapolated value of q based on first and second derivatives
 !-------------------------------------------------------------------------
-function get_extrapolated_q(dT, old_q, old_q_dot, old_q_double_dot) result(new_q)
-real(dp), intent(in)           :: dT
+function get_extrapolated_q(del_T, old_q, old_q_dot, old_q_double_dot) result(new_q)
+real(dp), intent(in)           :: del_T
 real(dp), intent(in)           :: old_q(:), old_q_dot(:)
 real(dp), intent(in), optional :: old_q_double_dot(:)
 real(dp)                       :: new_q(size(old_q))
 
 if (present(old_q_double_dot)) then
-   new_q(:) = old_q(:) + dT*old_q_dot(:) + dt**2*old_q_double_dot(:)/2._dp
+   new_q(:) = old_q(:) + del_T*old_q_dot(:) + del_T**2*old_q_double_dot(:)/2._dp
 else 
-   new_q(:) = old_q(:) + dT*old_q_dot(:)
+   new_q(:) = old_q(:) + del_T*old_q_dot(:)
 end if
 
 end function
---
+++ utils.mod
@@ -0,0 +1,29 @@
+GFORTRAN module version '10' created from src/aaa_common_utils.f90
+MD5:7849bf01d2604e6f83e17ce943f22acd -- If you edit this, you'll get what you deserve.
+
+(() () () () () () () () () () () () () () () () () () () () () () () ()
+() () ())
+
+()
+
+()
+
+()
+
+()
+
+()
+
+(2 'newunit' 'utils' '' 1 ((PROCEDURE UNKNOWN-INTENT MODULE-PROC DECL
+UNKNOWN 0 0 FUNCTION ALWAYS_EXPLICIT) (INTEGER 4 0 0 0 INTEGER ()) 3 0 (
+4) () 5 () () () 0 0)
+6 'utils' 'utils' '' 1 ((MODULE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN
+UNKNOWN 0 0) (UNKNOWN 0 0 0 0 UNKNOWN ()) 0 0 () () 0 () () () 0 0)
+4 'unit' '' '' 3 ((VARIABLE OUT UNKNOWN-PROC UNKNOWN UNKNOWN 0 0
+OPTIONAL DUMMY) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () () 0 () () () 0 0)
+5 'n' '' '' 3 ((VARIABLE UNKNOWN-INTENT UNKNOWN-PROC UNKNOWN UNKNOWN 0 0
+RESULT ALWAYS_EXPLICIT) (INTEGER 4 0 0 0 INTEGER ()) 0 0 () () 0 () () ()
+0 0)
+)
+
+('newunit' 0 2 'utils' 0 6)
