d3918d96b2d439e8d79804d38c647d37869d9df2
komahanb <komahan.cool@gmail.com>
2015-10-22 02:57:54 -0400

    solver system call

--- test/pend.f90
+++ test/pend.f90
@@ -10,10 +10,6 @@ program pendulum
 
   implicit none
 
-!  type(body) :: alpha
-  real(dp)   :: residual(12)
-  real(dp)   :: jacob(12,12)
-
   ! solver related variables
   integer neq,info(15),idid,lrw,liw,iwork(1000),ipar, my, ii
   double precision t, y(12), yprime(12), tout, rtol(12),atol(12), rwork(1055), rpar, h0
@@ -22,29 +18,30 @@ program pendulum
 
   write(*,*) "Setting up the test problem"
 
-  call create_body()
+  ! ******************************************************
+  ! (1) create a pendulum body
+  ! ******************************************************
 
-  print*, alpha
-  call jac
-  stop
+  call create_body()
+  
+  ! ******************************************************
+  ! (2) set the res and jacobian for the linear system
+  ! ******************************************************
 
   ! set number of equations to solve
   NEQ=12
 
-  IDID=0  !?
+  IDID=0  
 
   ! set tolerances
-  Rtol = 0.005
-  Atol = 0.005
+  Rtol = 0.05
+  Atol = 0.05
 
   ! intial time
   t=0.00
 
   ! final time
   tout=0.03
-
-
-  ! initial state
 
   ! info block
   do ii=1,15
@@ -53,21 +50,26 @@ program pendulum
   info(2)=1
   info(3)=1
 
+  ! derivatives automatically by numerical differences
+  info(5) = 1   ! Yes - Set INFO(5)=0   ! No  - Set INFO(5)=1 ! provide JAC
+
+  info(11) = 0 ! 1 = Y and Y prime are consistent ; 0 = not consistent compute automatically
+
   ! allocate size of work arrays
-  liw=1000
-  lrw=1055 
-
-
-  ! (1) create a pendulum body
-
-  ! (2) set the res and jacobian for the linear system
+  liw=1000;  lrw=1055 ;
 
   ! (3) call the solver
-  write(*,*) '***************'
-  write(*,*) 'time,                              positions     ' 
-  do  while (t .le. tout) 
-     call ddassl(res,neq,t,y,yprime,tout,info,rtol,atol, idid,rwork,lrw,iwork,liw,rpar,ipar,jac)
-     write(*,'(8f13.8)') t,y(1),y(2),y(3),y(4),y(5),y(6),y(7)
+  write(*,*) '************************************************************************************&
+       &*************************************************************************************'
+  write(*,*) '       Time', '            q1', '           q2', '          q3', '            q4', &
+       &'          q5', '            q6', '           q7', '          q8',&
+       & '          q9', '           q10', '         q11',  '           q12'
+  write(*,*) '************************************************************************************&
+       &*************************************************************************************'
+  do while (t .le. tout) 
+     call ddassl(res,neq,t,y,yprime,tout,info,rtol,atol,&
+          & idid,rwork,lrw,iwork,liw,rpar,ipar,jac)
+     write(*,'(8f13.2)') t,y(1),y(2),y(3),y(4),y(5),y(6),y(7),y(8),y(9),y(10),y(11),y(12)
   end do
 
 end program pendulum
@@ -80,7 +82,6 @@ subroutine res(t,y,yprime,delta,ires,rpar,ipar)
   use constants
   use utils,only:get_vector_elements
   use body_mod,only:alpha, R_rigid
-!  use jacobian_mod, only: res_rigid
   implicit none
 
   real(dp)  :: t
@@ -103,24 +104,59 @@ subroutine res(t,y,yprime,delta,ires,rpar,ipar)
 
   !?? get the residual
   delta = get_vector_elements(R_rigid(alpha),4)
-  print*, "residual vector = ", delta
+!  print*, "residual vector = ", delta
+
+  call disp(t,delta)
 
 end subroutine res
 
 !********************************
 ! implementation of the jacobian
 !********************************
-subroutine jac
+subroutine jac(t,y,yprime,pd,cj,rpar,ipar)
+!!$C  to define the matrix of partial derivatives
+!!$C             PD=DG/DY+CJ*DG/DYPRIME
+!!$C         CJ is a scalar which is input to JAC.
+!!$C         For the given values of T,Y,YPRIME, the
+!!$C         subroutine must evaluate the non-zero partial
+!!$C         derivatives for each equation and each solution
+!!$C         component, and store these values in the
+!!$C         matrix PD.  The elements of PD are set to zero
+!!$C         before each call to JAC so only non-zero elements
+!!$C         need to be defined.
+!!$C
+!!$C         Subroutine JAC must not alter T,Y,(*),YPRIME(*), or CJ.
+!!$C         You must declare the name JAC in an EXTERNAL statement in
+!!$C         your program that calls DDASSL.  You must dimension Y,
+!!$C         YPRIME and PD in JAC.
+
   use constants,only:dp,sp
   use body_mod,only:alpha
   use jacobian_mod,only:jac_rigid
-  real(dp) :: a
+  implicit none
+  real(dp)     :: cj
+  real(dp)     :: t
+  real(dp)     :: y(12)
+  real(dp)     :: yprime(12)
+  integer(sp)  :: ires
+  real(dp)     :: rpar(*)
+  integer(sp)  :: ipar(*)
+  real(dp)     :: pd(12,12)
+  integer(sp)  :: i,j,k
+
   ! JACOBIAN  = jac_rigid(alpha,a) !?? implement jacobian
-  a = 0.1d0
+ ! print*,cj 
+!  stop
+  cj = 0.1
+  pd = jac_rigid (alpha, cj)
 
-  print*, "JAC:", jac_rigid (alpha, a)
+  print*,  shape(pd)
+  
+  call disp_mat(t, pd, 12, 12)
 
-  stop"where is the impl"
+  stop
+
+  !  stop"where is the impl"
 
 end subroutine jac
 
@@ -170,10 +206,10 @@ subroutine create_body()
 !!$
 
   ! define positions of body frame from inertial (q terms)
-  r       = (/ 2.0_dp, 2._dp, 2.0_dp /)
-  theta   = (/ deg2rad(30.0d0), deg2rad(45.0d0), deg2rad(60.0d0) /)
-  v     = (/ 0.0d0, 0.0d0, 0.0d0 /)
-  omega = (/ 0.0d0, 0.0d0, 1.0d0 /)
+  r       = (/ 0.0_dp, 0._dp, 0.0_dp /)
+  theta   = (/ deg2rad(10.0d0), deg2rad(0.0d0), deg2rad(0.0d0) /)
+  v     = (/ 1.0d0, 0.0d0, 0.0d0 /)
+  omega = (/ 1.0d0, 0.0d0, 0.0d0 /)
 
   alpha%r         = vector(r)
   alpha%theta     = vector(theta)
@@ -208,3 +244,36 @@ subroutine create_body()
   alpha%gr = alpha%J*alpha%omega_dot
 
 end subroutine create_body
+
+
+
+subroutine disp(t,y)
+
+  use constants,only:dp,sp
+  implicit none
+
+  real(dp)    :: t, y(12)
+  integer(sp) :: i
+
+  write(*,'(13f13.2)') t, (y(i),i=1,12)
+
+
+end subroutine disp
+
+
+subroutine disp_mat(t, A,m,n)
+
+  use constants, only:dp,sp
+  implicit none
+
+  real(dp), intent(in) :: t
+  integer(sp)          :: i, j, m, n
+  real(dp)             :: A(m,n)
+
+
+  do i = 1, n
+     write(*,'(13f13.2)') t, (A(j,i),j=1,m)
+  end do
+
+
+end subroutine disp_mat
