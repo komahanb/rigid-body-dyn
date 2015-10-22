ebff8b8d6e2617b24ea119c24fe4560377c12535
komahan <komahan.cool@gmail.com>
2015-10-22 19:28:51 -0400

    rot and trans matrices transposed

--- src/matrix_utils.f90
+++ src/matrix_utils.f90
@@ -74,15 +74,15 @@ contains
     real(dp)                :: CBI(num_spat_dim, num_spat_dim)
 
     CBI(1,1) =  cos(theta(2))*cos(theta(3)) + sin(theta(1))*sin(theta(2))*sin(theta(3))
-    CBI(2,1) =  cos(theta(1))*sin(theta(3))
-    CBI(3,1) = -sin(theta(2))*cos(theta(3)) + sin(theta(1))*cos(theta(2))*sin(theta(3))
+    CBI(2,1) = -cos(theta(2))*sin(theta(3)) + sin(theta(1))*sin(theta(2))*cos(theta(3))
+    CBI(3,1) =  cos(theta(1))*sin(theta(2))
 
-    CBI(1,2) = -cos(theta(2))*sin(theta(3)) + sin(theta(1))*sin(theta(2))*cos(theta(3))
+    CBI(1,2) =  cos(theta(1))*sin(theta(3))
     CBI(2,2) =  cos(theta(1))*cos(theta(3))
-    CBI(3,2) =  sin(theta(2))*sin(theta(3)) + sin(theta(1))*cos(theta(2))*cos(theta(3))
+    CBI(3,2) = -sin(theta(1))
 
-    CBI(1,3) =  cos(theta(1))*sin(theta(2))
-    CBI(2,3) = -sin(theta(1))
+    CBI(1,3) = -sin(theta(2))*cos(theta(3)) + sin(theta(1))*cos(theta(2))*sin(theta(3))
+    CBI(2,3) =  sin(theta(2))*sin(theta(3)) + sin(theta(1))*cos(theta(2))*cos(theta(3))
     CBI(3,3) =  cos(theta(1))*cos(theta(2))
 
   end function CBI
@@ -97,15 +97,15 @@ contains
     real(dp)                :: SIB(num_spat_dim, num_spat_dim)
 
     SIB(1,1) = cos(theta(3))
-    SIB(2,1) = cos(theta(1))*sin(theta(3))
-    SIB(3,1) = 0.0_dp
-
     SIB(2,1) = -sin(theta(3))
-    SIB(2,2) = cos(theta(1))*cos(theta(3))
-    SIB(3,2) = 0.0_dp
-
     SIB(3,1) = 0.0_dp
+
+    SIB(1,2) = cos(theta(1))*sin(theta(3))
+    SIB(2,2) = cos(theta(1))*cos(theta(3))
     SIB(3,2) = -sin(theta(1)) 
+   
+    SIB(1,3) = 0.0_dp
+    SIB(2,3) = 0.0_dp
     SIB(3,3) = 1.0_dp
 
   end function SIB
