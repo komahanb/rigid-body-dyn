2c1a7d8ec32385c0c7630c6c5d6df3e710236cad
komahanb <komahan.cool@gmail.com>
2015-10-24 18:11:35 -0400

    LU solve routines added

---
+++ src/LUroutines.f90
@@ -0,0 +1,248 @@
+      subroutine lu(M,N) 
+  
+      implicit none
+      integer, intent(IN)   :: N
+      real*8 , intent(INOUT):: M(N,N)
+      integer:: i,j,k
+      
+      do j = 1,N
+       do i = j+1,N
+        if(abs(M(j,j)).lt.1.e-23)then
+          print*,'[STOP]The matrix is singular. LU factorization fails'
+         stop
+        endif
+         M(i,j) = M(i,j)/M(j,j)
+        do k = j+1,N
+         M(i,k) = M(i,k) - M(i,j) * M(j,k)
+        enddo
+       enddo
+      enddo
+   
+      return
+      
+      end subroutine lu
+
+      subroutine dgefa(a,lda,n,ipvt,info)
+      integer lda,n,ipvt(n),info
+      double precision a(lda,n)
+!
+!     dgefa factors a double precision matrix by gaussian elimination.
+!!
+!     on entry
+!
+!        a       double precision(lda, n)
+!                the matrix to be factored.
+!
+!        lda     integer
+!                the leading dimension of the array  a .
+!
+!        n       integer
+!                the order of the matrix  a .
+!
+!     on return
+!
+!        a       an upper triangular matrix and the multipliers
+!                which were used to obtain it.
+!                the factorization can be written  a = l*u  where
+!                l  is a product of permutation and unit lower
+!                triangular matrices and  u  is upper triangular.
+!
+!        ipvt    integer(n)
+!                an integer vector of pivot indices.
+!
+!        info    integer
+!                = 0  normal value.
+!                = k  if  u(k,k) .eq. 0.0 .  this is not an error
+!                     condition for this subroutine, but it does
+!                     indicate that dgesl or dgedi will divide by zero
+!                     if called.  use  rcond  in dgeco for a reliable
+!                     indication of singularity.
+!
+!     linpack. this version dated 08/14/78 .
+!     cleve moler, university of new mexico, argonne national lab.
+!
+!     subroutines and functions
+!
+!     blas daxpy,dscal,idamax
+!
+!     internal variables
+!
+      double precision t
+      integer idamax,j,k,kp1,l,nm1
+!
+!
+!     gaussian elimination with partial pivoting
+!
+      info = 0
+      nm1 = n - 1
+      if (nm1 .lt. 1) go to 70
+      do 60 k = 1, nm1
+         kp1 = k + 1
+!
+!        find l = pivot index
+!
+         l = idamax(n-k+1,a(k,k),1) + k - 1
+         ipvt(k) = l
+!
+!        zero pivot implies this column already triangularized
+!
+         if (a(l,k) .eq. 0.0d0) go to 40
+!
+!           interchange if necessary
+!
+            if (l .eq. k) go to 10
+               t = a(l,k)
+               a(l,k) = a(k,k)
+               a(k,k) = t
+   10       continue
+!
+!           compute multipliers
+!
+            t = -1.0d0/a(k,k)
+            call dscal(n-k,t,a(k+1,k),1)
+!
+!           row elimination with column indexing
+!
+            do 30 j = kp1, n
+               t = a(l,j)
+               if (l .eq. k) go to 20
+                  a(l,j) = a(k,j)
+                  a(k,j) = t
+   20          continue
+               call daxpy(n-k,t,a(k+1,k),1,a(k+1,j),1)
+   30       continue
+         go to 50
+   40    continue
+            info = k
+   50    continue
+   60 continue
+   70 continue
+      ipvt(n) = n
+      if (a(n,n) .eq. 0.0d0) info = n
+      return
+      end
+
+
+
+
+
+
+
+
+      subroutine dgesl(a,lda,n,ipvt,b,job)
+      integer lda,n,ipvt(n),job
+      double precision a(lda,n),b(n)
+!
+!     dgesl solves the double precision system
+!     a * x = b  or  trans(a) * x = b
+!     using the factors computed by dgeco or dgefa.
+!
+!     on entry
+!
+!        a       double precision(lda, n)
+!                the output from dgeco or dgefa.
+!
+!        lda     integer
+!                the leading dimension of the array  a .
+!
+!        n       integer
+!                the order of the matrix  a .
+!
+!        ipvt    integer(n)
+!                the pivot vector from dgeco or dgefa.
+!
+!        b       double precision(n)
+!                the right hand side vector.
+!
+!        job     integer
+!                = 0         to solve  a*x = b ,
+!                = nonzero   to solve  trans(a)*x = b  where
+!                            trans(a)  is the transpose.
+!
+!     on return
+!
+!        b       the solution vector  x .
+!
+!     error condition
+!
+!        a division by zero will occur if the input factor contains a
+!        zero on the diagonal.  technically this indicates singularity
+!        but it is often caused by improper arguments or improper
+!        setting of lda .  it will not occur if the subroutines are
+!        called correctly and if dgeco has set rcond .gt. 0.0
+!        or dgefa has set info .eq. 0 .
+!
+!     to compute  inverse(a) * c  where  c  is a matrix
+!     with  p  columns
+!           call dgeco(a,lda,n,ipvt,rcond,z)
+!           if (rcond is too small) go to ...
+!           do 10 j = 1, p
+!              call dgesl(a,lda,n,ipvt,c(1,j),0)
+!        10 continue
+!
+!     linpack. this version dated 08/14/78 .
+!     cleve moler, university of new mexico, argonne national lab.
+!
+!     subroutines and functions
+!
+!     blas daxpy,ddot
+!
+!     internal variables
+!
+      double precision ddot,t
+      integer k,kb,l,nm1
+!
+      nm1 = n - 1
+      if (job .ne. 0) go to 50
+!
+!        job = 0 , solve  a * x = b
+!        first solve  l*y = b
+!
+         if (nm1 .lt. 1) go to 30
+         do 20 k = 1, nm1
+            l = ipvt(k)
+            t = b(l)
+            if (l .eq. k) go to 10
+               b(l) = b(k)
+               b(k) = t
+   10       continue
+            call daxpy(n-k,t,a(k+1,k),1,b(k+1),1)
+   20    continue
+   30    continue
+!
+!        now solve  u*x = y
+!
+         do 40 kb = 1, n
+            k = n + 1 - kb
+            b(k) = b(k)/a(k,k)
+            t = -b(k)
+            call daxpy(k-1,t,a(1,k),1,b(1),1)
+   40    continue
+      go to 100
+   50 continue
+!
+!        job = nonzero, solve  trans(a) * x = b
+!        first solve  trans(u)*y = b
+!
+         do 60 k = 1, n
+            t = ddot(k-1,a(1,k),1,b(1),1)
+            b(k) = (b(k) - t)/a(k,k)
+   60    continue
+!
+!        now solve trans(l)*x = y
+!
+         if (nm1 .lt. 1) go to 90
+         do 80 kb = 1, nm1
+            k = n - kb
+            b(k) = b(k) + ddot(n-k,a(k+1,k),1,b(k+1),1)
+            l = ipvt(k)
+            if (l .eq. k) go to 70
+               t = b(l)
+               b(l) = b(k)
+               b(k) = t
+   70       continue
+   80    continue
+   90    continue
+  100 continue
+      return
+      end
