ee2c7759aba2790db1447afb64cb6e4f7009ba15
komahan <komahan.cool@gmail.com>
2015-11-09 20:31:55 -0500

    added a module for finite-difference a function to get the derivatives

--- src/finite_diff.f90
+++ src/finite_diff.f90
@@ -27,85 +27,91 @@
 ! 
 ! The partial derivative of R with respect to q would be a 3x3 matrix
 ! In general the matrix size would be: [neq x nvar]
+!---------------------------------------------------------------------!
+! Note:
+! 
+!  The approximation of derivatives with finite-difference will cost
+!  (N + 1) solutions of the governing equation which may not be 
+!  readily available. 
+! 
+!  Also FD approximations are dependent on the step size chosen
 !
 !********************************************************************!
 module finite_diff
 
   implicit none
+!!$  
+!!$  interface finite_diff
+!!$     module procedure finite_diff_vector !, finite_diff_scalar
+!!$  end interface finite_diff
 
-!!$  private
-!!$
-!!$  public
-!!$
-!!$  interface
-!!$
-!!$  end interface
-
-  !-------------------------------------------------------------------!
-  !  Define constants to manage precision [TUNABLE]
-  !-------------------------------------------------------------------!
-  integer, parameter      :: sp = kind(0.0)    ! single precision
-  integer(sp), parameter  :: dp = kind(0.0d0)  ! double precision
-
-  real(dp) :: h = 1.0d-10 ! step size for FD
-
-  interface
-     ! Signature of the function that the user must implement
-!!$     function R(q)
-!!$       integer, parameter      :: sp = kind(0.0)    ! single precision
-!!$       integer(sp), parameter  :: dp = kind(0.0d0)  ! double precision
-!!$       real(dp), dimension(:), intent(in) :: q
-!!$       real(dp), dimension(size(q)) :: R
-!!$     end function R
-  end interface
+  interface finite_difference
+     module procedure finite_diff_vector
+  end interface finite_difference
 
 contains
 
   !-------------------------------------------------------------------!
   ! The governing equation or function to be differentiated
+  ! Input:
+  ! The variables q as an array
+  ! The finite difference step size, optional
   !-------------------------------------------------------------------!
-  function FD(q, dh)
+  function finite_diff_vector(q, dh_in) result(jac)
 
     integer, parameter                    :: sp = kind(0.0)    ! single precision
     integer(sp), parameter                :: dp = kind(0.0d0)  ! double precision
 
+    ! input variables
     real(dp), dimension(:)                :: q            ! variables 
-    real(dp), dimension(size(q))          :: qtmp         ! temp storage
+    real(dp), optional                    :: dh_in           ! step size
 
-    
-    real(dp), dimension(size(q), size(q)) :: FD           ! jacobian
-    real(dp), optional                    :: dh           ! step size
-    real(dp), dimension(size(q))          :: F, Ftmp, dF  ! solution vector   
+    ! output
+    real(dp), dimension(size(q), size(q)) :: jac           ! jacobian
+    real(dp), dimension(size(q))          :: F, Ftmp, dF, qtmp
     integer(sp)                           :: k, N
 
-    ! R is to be implemented by the using program
-    real(dp), external           :: R
+    real(dp)                              :: dh = 1.0e-10_dp !   size
 
-    if (present(dh)) then
-       write(*,*) "Using the supplied time step: ", dh
-    else
+    ! R is to be implemented by the using program
+    external                              :: residual
+
+    !-----------------------------------------------------------------!
+    !--------------------- finite difference step --------------------!
+    !-----------------------------------------------------------------!
+    if (present(dh_in)) then ! if the step is supplied
+       dh = dh_in
+       write(*,*) "Using the supplied step-size: ", dh
+    else ! if the step is not supplied
        dh = 1.0e-10_dp
-       write(*,*) "Using the default time step: ", dh
+       write(*,*) "Using the default time step-size: ", dh
     end if
 
-    ! compute original solution and perform init tasks
-    F = R(q); qtmp = q; dF = 0.0_dp;
+    if (dh.eq.0.0_dp) stop " >> Wrong FD step-size! Please check!"
 
-    N = size(q); 
-    var: do  k = 1 , N ! loop through all variables     
+    !-----------------------------------------------------------------!
+    !--------------- compute original sol (unperturbed)---------------!
+    !-----------------------------------------------------------------!
+    call residual(q, F)
+
+    !-----------------------------------------------------------------!
+    !--------------- compute perturbed sol----------------------------!
+    !-----------------------------------------------------------------!
+    qtmp = q;     dF = 0.0_dp;     N = size(q); 
+    variables: do  k = 1 , N ! loop through all variables 
+       ! N residual calls are made in this loop
        ! perturb k-th variable
        qtmp( k ) = qtmp (k)  + dh
        ! Find the perturbed solution
-       Ftmp       = R(q)
+       call residual(qtmp, Ftmp)
        ! derivative of R wrt the k-th variables: partial R/partial q_k
        dF =  (Ftmp - F)/dh
        ! store in the jacobian matrix
-       FD (:, k) = dF
-       ! reset the variable to original state 
-       ! and get ready to perturb the next variable
+       jac (:, k) = dF
+       ! reset the variable to original state  and get ready to perturb the next variable
        qtmp = q
-    end do var
-
-  end function FD
+    end do variables
+    
+  end function finite_diff_vector
 
 end module finite_diff
